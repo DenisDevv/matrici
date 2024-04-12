9f8525ac3eaa0ec52f649bc749116a2cdc713c45
DenisDevv <karajedenis@gmail.com>
2024-04-12 12:58:18 +0200

    Creata funzione per sostituire la parta inferiore alla diagonale e superiore alla diagonale con 0

--- Es7.java
+++ Es7.java
@@ -1,3 +1,29 @@
 public class Es7 {
-    
+    public static void riempiMatrice(int[][] matrice) {
+        int n = matrice.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i >= j) {
+                    matrice[i][j] = 1;
+                } else {
+                    matrice[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[][] matrice = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+        };
+        
+        riempiMatrice(matrice);
+        for (int i = 0; i < matrice.length; i++) {
+            for (int j = 0; j < matrice[i].length; j++) {
+                System.out.print(matrice[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
